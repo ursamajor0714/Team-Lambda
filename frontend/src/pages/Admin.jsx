d385e316bac604558c8447e8e1c61f3823ecfa80
ursamajor0714 <ohyou66@gmail.com>
2026-06-08 01:03:03 +0900

    Update Admin.jsx

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -12,7 +12,7 @@
 // =============================================================================
 
 import { useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, Link  } from 'react-router-dom'
 import { useAuth } from '../context/AuthContext'
 import { api } from '../api/client'
 
